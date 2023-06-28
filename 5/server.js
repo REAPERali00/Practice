6e3556112205c99aaf71c10eded156af677dd03f
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2023-06-28 09:39:46 -0400

    5.4

--- 5/server.js
+++ 5/server.js
@@ -3,7 +3,14 @@ var app = express();
 
 //Enables access to the files in current directory? 
 app.use(express.static(__dirname));
+var messages = [
+    {name: 'Tim', message: 'Hi'}, 
+    {name: 'Alireza', message: 'Fuck you'}, 
 
+];
+app.get('/messages', (req, res)=>{
+    res.send(messages);
+});
 //Sets up the server 
 var server = app.listen(3000, ()=>{
     console.log('Server is listening on port ', server.address().port);

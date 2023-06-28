3fd4466738b6bbdef71cb1803fbabd85c3051049
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2023-06-28 14:51:22 -0400

    Beginning work with sockets

--- 5/server.js
+++ 5/server.js
@@ -1,16 +1,25 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
+var app = express(); //Creating an instance of express
 
 //Enables access to the files in current directory? 
 app.use(express.static(__dirname));
+app.use(bodyParser.json()); //This lets our body parser to know that json is coming along with our http request. 
+app.use(bodyParser.urlencoded({extended:false}));
 var messages = [
     {name: 'Tim', message: 'Hi'}, 
-    {name: 'Alireza', message: 'Fuck you'}, 
+    {name: 'Alireza', message: 'I am the developer'} 
 
 ];
 app.get('/messages', (req, res)=>{
     res.send(messages);
 });
+
+app.post('/messages', (req, res)=>{
+    messages.push(req.body);
+    res.sendStatus(200);
+});
+
 //Sets up the server 
 var server = app.listen(3000, ()=>{
     console.log('Server is listening on port ', server.address().port);

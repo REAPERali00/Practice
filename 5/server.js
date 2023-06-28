53bb6eedfdf0ca75a7628e4497701848750319f0
Alireza Jadidi <120317445+REAPERali00@users.noreply.github.com>
2023-06-28 15:29:44 -0400

    Added Socket

--- 5/server.js
+++ 5/server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express(); //Creating an instance of express
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http); //Setting up the io for the backend
+
 //Enables access to the files in current directory? 
 app.use(express.static(__dirname));
 app.use(bodyParser.json()); //This lets our body parser to know that json is coming along with our http request. 
@@ -20,7 +23,11 @@ app.post('/messages', (req, res)=>{
     res.sendStatus(200);
 });
 
-//Sets up the server 
-var server = app.listen(3000, ()=>{
+//Sets up the server, only for express
+// var server = app.listen(3000, ()=>{
+//     console.log('Server is listening on port ', server.address().port);
+// });
+
+var server = http.listen(3000, ()=>{
     console.log('Server is listening on port ', server.address().port);
 });
